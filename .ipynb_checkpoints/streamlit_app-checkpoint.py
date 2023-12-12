7b7d6ea3c5fe46147ed3c28fadff402cd11b2cd2
Anthony Kim <tkim1205@gmail.com>
2023-12-11 22:30:29 -0500

    add debug

--- .ipynb_checkpoints/streamlit_app-checkpoint.py
+++ .ipynb_checkpoints/streamlit_app-checkpoint.py
@@ -15,7 +15,17 @@ icbc_end = '[-icbc end-]'
 def main():
     st.title("Rewordify")
 
+    # PDF File Uploader
     pdf_file = st.file_uploader("Choose a PDF file", type='pdf', accept_multiple_files=False, disabled=False, label_visibility="visible")
+
+    # Debug
+    with st.expander("Debug mode"):
+        debug_pdf_text = st.toggle("Show extracted PDF Text", value"False")
+        debug_text_between_markers = st.toggle("Show text between markers", value"False")
+        debug_personal_info = st.toggle("Show personal data extracted", value"False")
+        debug_summary_text = st.toggle("Show summary text extracted", value"False")
+        debug_past_medical_text = st.toggle("Show past medical text extracted", value"False")
+        debug_chatgpt_prompt = st.toggle("Show ChatGPT prompt", value"False")
 
     if st.button("Rewordify"):
         try:
@@ -29,9 +39,25 @@ def main():
                     chatgpt_prompt = prompt_template + summary_text
                     
                     response = util.call_chatgpt(chatgpt_prompt, st.secrets["api_key"])
-                    
+
+                # Done
                 st.success('Done!')
-                st.write(name, age, gender)
+
+                # Debug
+                if debug_pdf_text:
+                    st.write(pdf_text)
+                if debug_text_between_markers:
+                    st.write(text_between_markers)
+                if debug_personal_info:
+                    st.write("Name:", name, "\nAge:", age, "\nGender:", gender)
+                if debug_summary_text:
+                    st.write(summary_text)
+                if debug_past_medical_text:
+                    st.write(past_medical_text)
+                if debug_chatgpt_prompt:
+                    st.write(chatgpt_prompt)
+
+                # Print Reponse
                 st.code(response + '\n\n' + past_medical_text, language="python")
             
             elif pdf_file is None:
