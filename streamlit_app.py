ce4e740dc05aa3daa031378f8a4d0c32994de90d
Anthony Kim <tkim1205@gmail.com>
2024-01-21 21:33:56 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,7 +20,9 @@ def main():
     # Model
     model = st.selectbox(
         '**ChatGPT Model**',
-        ('gpt-4-1106-preview', 'gpt-4', 'gpt-3.5-turbo'))
+        #('gpt-4-1106-preview', 'gpt-4', 'gpt-3.5-turbo')
+        ('gpt-3.5-turbo')
+        )
 
     # Prompt
     prompt_template = st.text_area(
