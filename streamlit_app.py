8910c2715aff23818454936e36baa7374128a9b3
Anthony Kim <tkim1205@gmail.com>
2024-02-20 13:18:24 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -243,7 +243,8 @@ def main():
                     with st.container(border=True):
                         st.markdown(combine_sections_new)
                     if st.button('Copy'):
-                        pyperclip.copy(combine_sections.replace('**', ''))
+                        combine_sections_copy = combine_sections.replace('**', '')
+                        pyperclip.copy(combine_sections_copy)
             
             elif pdf_file is None:
                 st.write("Please choose a valid PDF file")
