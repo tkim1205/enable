f0bd6c060673f3a02a6057804afdea7339d695ad
Anthony Kim <tkim1205@gmail.com>
2024-05-25 00:24:17 -0400

    Update util_v2.py

--- util_v2.py
+++ util_v2.py
@@ -384,7 +384,7 @@ def clean_section_text(api_key, model, section_text):
     Returns:
     - string: The section text cleansed
     """
-    chatgpt_prompt = "I am a medical assistant. Try to spell-check the text below. If you are unsure, leave the text as is. If the text is talking about an attached page, ignore.\n\nText below:\n\n" + section_text
+    chatgpt_prompt = "I am a medical assistant. Try to spell-check the text below. If you are unsure, leave the text as is. If the text is not medical related, remove the text.\n\nText below:\n\n" + section_text
     chatgpt_response = call_chatgpt(chatgpt_prompt, api_key, model)
     return chatgpt_response
 
